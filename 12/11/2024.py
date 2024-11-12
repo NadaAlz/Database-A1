ff85515b2ddf78116dcb3ccd3a1bbafc1bd43703
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-11-12 23:16:27 +0400

    Update 2024.py

--- 12/11/2024.py
+++ 12/11/2024.py
@@ -3,9 +3,15 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    passwd = ""
+    passwd = "",
+    database = "community"
 )
 
 mycursor = mydb.cursor()
 
+#mycursor.execute("CREATE TABLE building (name VARCHAR(50), floors INTEGER(200))")
 
+mycursor.execute("SHOW TABLES")
+
+for tb in mycursor:
+    print(tb)
