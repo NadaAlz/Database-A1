c47f9ce4fe20177683552bc532275dfeca33fde4
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-11-13 08:45:15 +0400

    Update 2024.py

--- 12/11/2024.py
+++ 12/11/2024.py
@@ -4,12 +4,14 @@ mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
     passwd = "",
-    database = "community"
+    database = "bank community"
 )
 
 mycursor = mydb.cursor()
 
-#mycursor.execute("CREATE TABLE building (name VARCHAR(50), floors INTEGER(200))")
+#mycursor.execute("CREATE DATABASE Community")
+
+mycursor.execute("CREATE TABLE building (name VARCHAR(50), floors INTEGER(200))")
 
 mycursor.execute("SHOW TABLES")
 
