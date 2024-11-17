c31cbef8a1e91adc7fd91095fcb4f56e2b0b259f
NadaAlz <165168524+NadaAlz@users.noreply.github.com>
2024-11-17 18:42:41 +0400

    Update 2024.py

--- 12/11/2024.py
+++ 12/11/2024.py
@@ -9,11 +9,49 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+#Creating database
+
 #mycursor.execute("CREATE DATABASE Community")
 
-mycursor.execute("CREATE TABLE building (name VARCHAR(50), floors INTEGER(200))")
+#creating tables
 
-mycursor.execute("SHOW TABLES")
+#mycursor.execute("CREATE TABLE building (name VARCHAR(50), floors INTEGER(200))")
 
-for tb in mycursor:
-    print(tb)
+#mycursor.execute("SHOW TABLES")
+
+
+#for tb in mycursor:
+ #   print(tb)
+
+ #Inserting values
+
+#sqlFormula = "INSERT INTO building (name, floors) VALUES (%s, %s)"
+#building1 = ("Qamar1",45)
+
+#mycursor.execute(sqlFormula, building1)
+
+#mydb.commit()
+
+#Selecting data
+
+#mycursor.execute("SELECT name FROM building")
+
+#yresult = mycursor.fetchall()
+
+#for row in myresult:
+ #  print(row)
+
+
+# Updating a record
+#update_query = "UPDATE building SET floors = %s WHERE name = %s"
+#data_to_update = (50, "Qamar1")  # Update floors to 50 for building 'Qamar1'
+
+#mycursor.execute(update_query, data_to_update)
+#mydb.commit()
+
+# Deleting a record
+#delete_query = "DELETE FROM building WHERE name = %s"
+#data_to_delete = ("Qamar1",)
+
+#mycursor.execute(delete_query, data_to_delete)
+#mydb.commit()
